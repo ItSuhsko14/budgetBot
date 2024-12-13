f0c71882f212569a97fc517d00c621f805759bea
Sushko Andriy <a.sushko@joinposter.com>
2024-12-13 18:11:36 +0200

    create webhook processing'

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-import logging
 import os
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from handlers.start_handler import start
 from handlers.message_handler import handle_message
 from handlers.button_handler import button
 from utils.logger import logger
-from data.chat_data import chat_data
 
+# Завантаження змінних середовища
 def load_env():
     if os.path.exists(".env"):
         with open(".env") as f:
             for line in f:
-                # Ігноруємо коментарі та порожні рядки
                 if line.strip() and not line.startswith("#"):
                     key, value = line.strip().split("=", 1)
                     os.environ[key] = value
 
-# Запуск завантаження змінних
+# Завантаження змінних
 load_env()
 
-# Отримання токена
-TOKEN = os.environ.get("TOKEN")
-
-def main():
-    logger.info("Запуск бота...")
-    application = Application.builder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    application.add_handler(CallbackQueryHandler(button))
-    logger.info("Бот працює!")
-    application.run_polling()
+# Конфігурація
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = int(os.getenv("PORT", 8443))
 
 if __name__ == "__main__":
-    main()
+    try:
+        logger.info("Запуск бота...")
+
+        # Ініціалізація додатку
+        application = Application.builder().token(TOKEN).build()
+
+        # Додавання обробників
+        application.add_handler(CommandHandler("start", start))
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+        application.add_handler(CallbackQueryHandler(button))
+
+        logger.info("Бот працює!")
+
+        # Встановлення вебхука
+        application.run_webhook(
+            listen="0.0.0.0",
+            port=PORT,
+            webhook_url=WEBHOOK_URL,
+        )
+    except Exception as e:
+        logger.error(f"Помилка запуску: {e}")
