96ef0d3a07fc81f74ff5ab7801bad9e366c50244
Sushko Andriy <a.sushko@joinposter.com>
2025-04-29 17:08:31 +0300

    remove web_hook path

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ if __name__ == "__main__":
             listen="0.0.0.0",
             port=PORT,
             webhook_url=WEBHOOK_URL,
-            webhook_path="/webhook"
         )
     except Exception as e:
         logger.error(f"Помилка запуску: {e}")
