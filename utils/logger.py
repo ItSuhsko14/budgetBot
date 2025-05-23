214517e3951f1f2c3681381b97a7275a666828bc
Sushko Andriy <a.sushko@joinposter.com>
2025-05-23 11:31:31 +0300

    add [budget] in logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,14 @@
 import logging
 import sys
 
+class BudgetFormatter(logging.Formatter):
+    def format(self, record):
+        record.msg = f"[budget] {record.msg}"
+        return super().format(record)
+
 def setup_logger():
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = BudgetFormatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
     logger = logging.getLogger()
